efe17fb5b3500f1eba986810ab31a2e50c9a66b7
Arina Zhukova <Azhukova19842005@yandex.ru>
2025-10-29 17:19:06 +0300

    Перезалив проекта для хакатона

---
+++ my-strapi-project/config/middlewares.ts
@@ -0,0 +1,32 @@
+
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: process.env.CORS_ORIGIN.split(','),
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
+
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
